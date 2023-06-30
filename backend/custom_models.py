848ac1f7ec5384663e4fe65fccb5208f487cde6a
Si-ris-B <51501708+Si-ris-B@users.noreply.github.com>
2023-06-30 21:30:35 +0545

    Add custom proxy model

--- backend/custom_models.py
+++ backend/custom_models.py
@@ -98,3 +98,29 @@ class SearchProxyModel(QSortFilterProxyModel):
     def filterAcceptsRow(self, sourceRow, sourceParent):
         idx = self.sourceModel().index(sourceRow, 0, sourceParent)
         return self._accept_index(idx)
+
+class CustomProxyModel(QSortFilterProxyModel):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self._filters = dict()
+
+    @property
+    def filters(self):
+        return self._filters
+
+    def setFilter(self, expresion, column):
+        if expresion:
+            self.filters[column] = expresion
+        elif column in self.filters:
+            del self.filters[column]
+        self.invalidateFilter()
+
+    def filterAcceptsRow(self, source_row, source_parent):
+        for column, expresion in self.filters.items():
+            text = self.sourceModel().index(source_row, column, source_parent).data()
+            regex = QRegExp(
+                expresion, QtCore.Qt.CaseInsensitive, QRegExp.RegExp
+            )
+            if regex.indexIn(text) == -1:
+                return False
+        return True
